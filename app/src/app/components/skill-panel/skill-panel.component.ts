42328f3da37822ce1efbb07d6ef329b0a0b0f15a
Gustavo Barros <gustavoo.barros07@gmail.com>
2023-09-28 20:27:30 -0300

    Brief about me added

--- app/src/app/components/skill-panel/skill-panel.component.ts
+++ app/src/app/components/skill-panel/skill-panel.component.ts
@@ -19,32 +19,10 @@ export class SkillPanelComponent implements OnInit {
   ngOnInit(): void {
     this.title = this.skill
     switch (this.skill) {
-      case "Java":
-        this.image = "assets/imgs/java-logo.webp"
+      case "About me":
+        this.image = "assets/imgs/perfil.jpeg"
         this.description = `
-        Nós somos do Clube Atlético Mineiro
-        Jogamos com muita raça e amor
-        Vibramos com alegria nas vitórias
-        Clube Atlético Mineiro
-        Galo Forte Vingador
-
-        Vencer, vencer, vencer
-        Este é o nosso ideal
-        Honramos o nome de Minas
-        No cenário esportivo mundial`
-        break
-      case "Python":
-        this.image = "assets/imgs/python-logo.png"
-        this.description = `
-        Lutar, lutar, lutar
-        Pelos gramados do mundo pra vencer
-        Clube Atlético Mineiro
-        Uma vez até morrer
-
-        Nós somos campeões do gelo
-        O nosso time é imortal
-        Nós somos campeões dos Campeões
-        Somos o orgulho do esporte nacional
+          That I love coding you already know, what you should know is that I aim to be the MVP for every team I'm in!
         `
         break
       }
